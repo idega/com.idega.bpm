707d236857b1662e7f3691d0087be17e5cb5d64e
Vytautas Civilis <civilis@idega.com>
2008-09-17 18:20:09 +0000

    getProcessDefinition(String processName) added

--- src/java/com/idega/bpm/bundle/exe/DefaultBPMProcessManager.java
+++ src/java/com/idega/bpm/bundle/exe/DefaultBPMProcessManager.java
@@ -1,6 +1,11 @@
 package com.idega.bpm.bundle.exe;
 
 
+import org.jbpm.JbpmContext;
+import org.jbpm.graph.def.ProcessDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.idega.jbpm.BPMContext;
 import com.idega.jbpm.exe.ProcessDefinitionW;
 import com.idega.jbpm.exe.ProcessInstanceW;
 import com.idega.jbpm.exe.ProcessManager;
@@ -8,15 +13,31 @@ import com.idega.jbpm.exe.TaskInstanceW;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  *
- * Last modified: $Date: 2008/09/17 13:09:02 $ by $Author: civilis $
+ * Last modified: $Date: 2008/09/17 18:20:09 $ by $Author: civilis $
  */
 public abstract class DefaultBPMProcessManager implements ProcessManager {
+	
+	@Autowired
+	private BPMContext bpmContext;
 
 	public ProcessDefinitionW getProcessDefinition(long pdId) {
 		
 		return createProcessDefinition(pdId);
+	}
+	
+	public ProcessDefinitionW getProcessDefinition(String processName) {
+		
+		JbpmContext jctx = getBpmContext().createJbpmContext();
+		
+		try {
+			ProcessDefinition pd = jctx.getGraphSession().findLatestProcessDefinition(processName);
+			return getProcessDefinition(pd.getId());
+			
+		} finally {
+			getBpmContext().closeAndCommit(jctx);
+		}
 	}
 
 	public ProcessInstanceW getProcessInstance(long piId) {
@@ -33,7 +54,6 @@ public abstract class DefaultBPMProcessManager implements ProcessManager {
 	
 //	synchronized because spring doesn't do it when autowiring beans
 	public synchronized ProcessDefinitionW createProcessDefinition(long pdId) {
-		
 		
 		ProcessDefinitionW pdw = createPDW();
 		pdw.setProcessDefinitionId(pdId);
@@ -61,5 +81,13 @@ public abstract class DefaultBPMProcessManager implements ProcessManager {
 		tiw.setTaskInstanceId(tiId);
 		
 		return tiw;
+	}
+
+	public BPMContext getBpmContext() {
+		return bpmContext;
+	}
+
+	public void setBpmContext(BPMContext bpmContext) {
+		this.bpmContext = bpmContext;
 	}
 }
