f3f85057adc4a3a6ddf2049cb0f8965d6d6681df
Valdas Zemaitis <valdas@idega.com>
2008-09-26 13:39:00 +0000

    *** empty log message ***

---
+++ src/java/com/idega/bpm/BPMConstants.java
@@ -0,0 +1,9 @@
+package com.idega.bpm;
+
+import com.idega.util.CoreConstants;
+
+public class BPMConstants {
+
+	public static final String PDF_OF_XFORMS_PATH_IN_SLIDE = CoreConstants.CONTENT_PATH + "/xforms/pdf/";
+	
+}
