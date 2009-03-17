7e301f85c8a4ecfef768987dfa9c2c0b7c3b9789
Vytautas Civilis <civilis@idega.com>
2009-03-17 13:59:19 +0000

    throw exception in getProcessDefinition(final String processName) if no process definition found

--- src/java/com/idega/bpm/exe/ProcessManagerAbstractImpl.java
+++ src/java/com/idega/bpm/exe/ProcessManagerAbstractImpl.java
@@ -20,154 +20,157 @@ import com.idega.jbpm.exe.TaskInstanceW;
 import com.idega.util.ListUtil;
 
 /**
- * abstract implementation of ProcessManager. Default behavior is that bean
- * container (e.g. spring) instantiates this and proxies the abstract methods,
- * which create wanted bpm wrapper instances
+ * abstract implementation of ProcessManager. Default behavior is that bean container (e.g. spring)
+ * instantiates this and proxies the abstract methods, which create wanted bpm wrapper instances
  * 
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
- * 
- *          Last modified: $Date: 2009/01/25 15:44:13 $ by $Author: civilis $
+ * @version $Revision: 1.2 $ Last modified: $Date: 2009/03/17 13:59:19 $ by $Author: civilis $
  */
 public abstract class ProcessManagerAbstractImpl implements ProcessManager {
-
+	
 	private String managerType;
 	@Autowired
 	private BPMContext bpmContext;
-
+	
 	@Autowired
 	private CaseManagersProvider caseManagersProvider;
-
+	
 	public ProcessDefinitionW getProcessDefinition(long pdId) {
-
+		
 		return createProcessDefinition(pdId);
 	}
-
+	
 	public ProcessDefinitionW getProcessDefinition(final String processName) {
-
+		
 		return getBpmContext().execute(new JbpmCallback() {
-
+			
 			public Object doInJbpm(JbpmContext context) throws JbpmException {
 				ProcessDefinition pd = context.getGraphSession()
-						.findLatestProcessDefinition(processName);
+				        .findLatestProcessDefinition(processName);
+				
+				if (pd == null)
+					throw new IllegalStateException(
+					        "Process definition not deployed by the process name = "
+					                + processName);
+				
 				return getProcessDefinition(pd.getId());
 			}
 		});
 	}
-
+	
 	public ProcessInstanceW getProcessInstance(long piId) {
-
+		
 		return createProcessInstance(piId);
 	}
-
+	
 	public TaskInstanceW getTaskInstance(long tiId) {
-
+		
 		return createTaskInstance(tiId);
 	}
-
+	
 	public TaskInstanceW getTaskInstance(TaskInstance ti) {
-
+		
 		TaskInstanceW tiw = createTaskInstance(ti.getId());
 		tiw.setTaskInstance(ti);
 		return tiw;
 	}
-
+	
 	/**
 	 * method injected by bean container
 	 * 
 	 * @return
 	 */
 	protected abstract ProcessDefinitionW createPDW();
-
+	
 	// synchronized because spring doesn't do it when autowiring beans (TODO: is
 	// this needed?)
 	public synchronized ProcessDefinitionW createProcessDefinition(long pdId) {
-
+		
 		ProcessDefinitionW pdw = createPDW();
 		pdw.setProcessDefinitionId(pdId);
-
+		
 		return pdw;
 	}
-
+	
 	/**
 	 * method injected by bean container
 	 * 
 	 * @return
 	 */
 	protected abstract ProcessInstanceW createPIW();
-
+	
 	// synchronized because spring doesn't do it when autowiring beans (TODO: is
 	// this needed?)
 	public synchronized ProcessInstanceW createProcessInstance(long piId) {
-
+		
 		ProcessInstanceW piw = createPIW();
 		piw.setProcessInstanceId(piId);
-
+		
 		return piw;
 	}
-
+	
 	/**
 	 * method injected by bean container
 	 * 
 	 * @return
 	 */
 	protected abstract TaskInstanceW createTIW();
-
+	
 	// synchronized because spring doesn't do it when autowiring beans (TODO: is
 	// this needed?)
 	public synchronized TaskInstanceW createTaskInstance(long tiId) {
-
+		
 		TaskInstanceW tiw = createTIW();
 		tiw.setTaskInstanceId(tiId);
 		tiw.setProcessManager(this);
-
+		
 		return tiw;
 	}
-
+	
 	public BPMContext getBpmContext() {
 		return bpmContext;
 	}
-
+	
 	public void setBpmContext(BPMContext bpmContext) {
 		this.bpmContext = bpmContext;
 	}
-
+	
 	public CaseManagersProvider getCaseManagersProvider() {
 		return caseManagersProvider;
 	}
-
+	
 	public void setCaseManagersProvider(
-			CaseManagersProvider caseManagersProvider) {
+	        CaseManagersProvider caseManagersProvider) {
 		this.caseManagersProvider = caseManagersProvider;
 	}
-
+	
 	// FIXME: wrong place for this, refactor
 	public List<ProcessDefinitionW> getAllProcesses() {
 		List<CaseManager> caseManagers = getCaseManagersProvider()
-				.getCaseManagers();
+		        .getCaseManagers();
 		if (ListUtil.isEmpty(caseManagers)) {
 			return null;
 		}
-
+		
 		List<ProcessDefinitionW> allProcesses = new ArrayList<ProcessDefinitionW>();
 		for (CaseManager caseManager : caseManagers) {
 			List<Long> caseProcesses = caseManager
-					.getAllCaseProcessDefinitions();
-
+			        .getAllCaseProcessDefinitions();
+			
 			if (!ListUtil.isEmpty(caseProcesses)) {
 				for (Long id : caseProcesses) {
 					allProcesses.add(getProcessDefinition(id));
 				}
 			}
 		}
-
+		
 		return allProcesses;
 	}
-
+	
 	public String getManagerType() {
 		return managerType != null ? managerType : "default";
 	}
-
+	
 	public void setManagerType(String managerType) {
 		this.managerType = managerType;
 	}
