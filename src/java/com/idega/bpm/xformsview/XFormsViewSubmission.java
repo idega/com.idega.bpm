0f1ee4276d18fd6da9b689cdfe2a3db2e0680fdf
Vytautas Civilis <civilis@idega.com>
2009-05-05 14:07:25 +0000

    using XFormSubmissionInstance and XFOrmParameters

--- src/java/com/idega/bpm/xformsview/XFormsViewSubmission.java
+++ src/java/com/idega/bpm/xformsview/XFormsViewSubmission.java
@@ -3,42 +3,47 @@ package com.idega.bpm.xformsview;
 import java.util.Map;
 
 import org.chiba.xml.xforms.core.Submission;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
+import com.idega.block.form.submission.XFormSubmissionInstance;
 import com.idega.jbpm.variables.Converter;
 import com.idega.jbpm.view.ViewSubmissionImpl;
-import com.idega.util.URIUtil;
-import com.idega.xformsmanager.util.FormManagerUtil;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
- * 
- *          Last modified: $Date: 2008/12/09 02:49:00 $ by $Author: civilis $
+ * @version $Revision: 1.2 $ Last modified: $Date: 2009/05/05 14:07:25 $ by $Author: civilis $
  */
 public class XFormsViewSubmission extends ViewSubmissionImpl {
-
+	
 	private Converter converter;
-
+	
 	public void setSubmission(Submission submission, Node submissionInstance) {
-
-		Element paramsEl = FormManagerUtil
-				.getFormParamsElement(submissionInstance);
-
-		Map<String, String> parameters = paramsEl == null ? new URIUtil(null)
-				.getParameters() : new URIUtil(paramsEl.getTextContent())
-				.getParameters();
-
+		
+		Map<String, String> parameters = new XFormSubmissionInstance(
+		        submissionInstance).getParameters();
+		
 		populateParameters(parameters);
 		populateVariables(getConverter().convert(submissionInstance));
 	}
-
+	
 	public Converter getConverter() {
 		return converter;
 	}
-
+	
 	public void setConverter(Converter converter) {
 		this.converter = converter;
+	}
+	
+	@Override
+	public Long getTaskInstanceId() {
+		Long taskInstanceId = super.getTaskInstanceId();
+		
+		if (taskInstanceId == null) {
+			
+			Map<String, String> params = resolveParameters();
+			taskInstanceId = new XFormParameters(params).getTaskInstance();
+		}
+		
+		return taskInstanceId;
 	}
 }
