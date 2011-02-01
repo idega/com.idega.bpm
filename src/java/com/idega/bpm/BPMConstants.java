1641a09408bfe0f7cd8446ee3168bac298d2813d
Valdas Žemaitis <valdas@idega.com>
2011-02-01 14:15:48 +0200

    New constant

--- src/java/com/idega/bpm/BPMConstants.java
+++ src/java/com/idega/bpm/BPMConstants.java
@@ -10,5 +10,7 @@ public class BPMConstants {
 	public static final String TEMP_PDF_OF_XFORMS_PATH_IN_SLIDE = CoreConstants.PUBLIC_PATH + "/xforms/pdf/temp/";
 	
 	public static final String TASK_CUSTOM_NAME_META_DATA = "BPM_TASK_CUSTOM_NAME",
-								TASK_CUSTOM_NAME_SEPARATOR = "@BPM_TASK_CUSTOM_NAME@";
+								TASK_CUSTOM_NAME_SEPARATOR = "@BPM_TASK_CUSTOM_NAME@",
+								
+								BPM_PROCESS_HANDLER_VARIABLE = "bpm_process_handler_variable";
 }
