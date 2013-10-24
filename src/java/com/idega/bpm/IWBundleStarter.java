3eb2fb6c4350c5e2fd0895d5767c356ecfd789ef
Valdas Žemaitis <valdas@idega.com>
2013-10-24 16:04:39 +0300

    Caching mail variables

--- src/java/com/idega/bpm/IWBundleStarter.java
+++ src/java/com/idega/bpm/IWBundleStarter.java
@@ -1,17 +1,30 @@
 package com.idega.bpm;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.idega.bpm.xform.BPMXFormPersistenceServiceImpl;
 import com.idega.idegaweb.IWBundle;
 import com.idega.idegaweb.IWBundleStartable;
+import com.idega.jbpm.data.VariableInstanceQuerier;
 import com.idega.util.CoreUtil;
 import com.idega.util.expression.ELUtil;
 
 public class IWBundleStarter implements IWBundleStartable {
 
-	@Autowired
+	@Autowired(required = false)
 	private BPMXFormPersistenceServiceImpl xformPersistenceService;
+
+	@Autowired
+	private VariableInstanceQuerier querier;
+
+	private VariableInstanceQuerier getVariableInstanceQuerier() {
+		if (querier == null) {
+			ELUtil.getInstance().autowire(this);
+		}
+		return querier;
+	}
 
 	private BPMXFormPersistenceServiceImpl getXFormPersistenceService() {
 		if (xformPersistenceService == null) {
@@ -29,6 +42,17 @@ public class IWBundleStarter implements IWBundleStartable {
 	public void start(IWBundle starterBundle) {
 		if (starterBundle.getApplication().getSettings().getBoolean("xform.bind_submissions_with_bpm", Boolean.TRUE)) {
 			getXFormPersistenceService().doBindSubmissionsWithBPM();
+		}
+
+		try {
+			getVariableInstanceQuerier().loadVariables(Arrays.asList(
+					BPMConstants.VAR_SUBJECT,
+					BPMConstants.VAR_TEXT,
+					BPMConstants.VAR_FROM,
+					BPMConstants.VAR_FROM_ADDRESS
+			));
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
